9019b62cccb290225b2d42a0eb4dcf9261203862
Júllia R Avellar <125517387+juravellar@users.noreply.github.com>
2025-06-25 21:20:23 -0300

    feat: integrate timezone support for doctor availability times in upsert-doctor action

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
@@ -13,6 +14,7 @@ import { actionClient } from "@/lib/next-safe-action";
 import { upsertDoctorSchema } from "./schema";
 
 dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
@@ -20,16 +22,16 @@ export const upsertDoctor = actionClient
     const availableFromTime = parsedInput.availableFromTime; // 15:30:00
     const availableToTime = parsedInput.availableToTime; // 16:00:00
 
-    const availableFromTimeUTC = dayjs()
-      .set("hour", parseInt(availableFromTime.split(":")[0]))
-      .set("minute", parseInt(availableFromTime.split(":")[1]))
-      .set("second", parseInt(availableFromTime.split(":")[2]))
-      .utc();
-    const availableToTimeUTC = dayjs()
-      .set("hour", parseInt(availableToTime.split(":")[0]))
-      .set("minute", parseInt(availableToTime.split(":")[1]))
-      .set("second", parseInt(availableToTime.split(":")[2]))
-      .utc();
+    const availableFromTimeLocal = dayjs.tz(
+      availableFromTime,
+      "HH:mm:ss",
+      "America/Sao_Paulo",
+    );
+    const availableToTimeLocal = dayjs.tz(
+      availableToTime,
+      "HH:mm:ss",
+      "America/Sao_Paulo",
+    );
 
     const session = await auth.api.getSession({
       headers: await headers(),
@@ -46,15 +48,15 @@ export const upsertDoctor = actionClient
         ...parsedInput,
         id: parsedInput.id,
         clinicId: session?.user.clinic?.id,
-        availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
-        availableToTime: availableToTimeUTC.format("HH:mm:ss"),
+        availableFromTime: availableFromTimeLocal.format("HH:mm:ss"),
+        availableToTime: availableToTimeLocal.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
         set: {
           ...parsedInput,
-          availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
-          availableToTime: availableToTimeUTC.format("HH:mm:ss"),
+          availableFromTime: availableFromTimeLocal.format("HH:mm:ss"),
+          availableToTime: availableToTimeLocal.format("HH:mm:ss"),
         },
       });
     revalidatePath("/doctors");
